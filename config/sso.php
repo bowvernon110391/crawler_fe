64e9f77353f0da87f164503f26b147f4cd130d2c
Bow Vernon <bowvernon110391@gmail.com>
2022-02-21 14:55:28 +0700

    tidy config sso

--- config/sso.php
+++ config/sso.php
@@ -1,9 +1,12 @@
 <?php
 
 return [
+    // these parameters are used in production (when spawning REAL BROKER)
     'url' => env('SSO_URL', ''),
     'broker' => env('SSO_BROKER', 0),
     'secret' => env('SSO_SECRET', '123456'),
     'login_url' => env('SSO_LOGIN_URL', 'https://lmgtfy?q=smegma'),
+
+    // this is used to spawn mock user
     'mock_user_id' => env('SSO_MOCK_USER_ID', 1)
 ];
