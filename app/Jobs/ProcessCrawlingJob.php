47ce516c183599680327c6c9832a55569cbd85dc
Bow Vernon <bowvernon110391@gmail.com>
2022-03-04 09:48:35 +0700

    setup queue

--- app/Jobs/ProcessCrawlingJob.php
+++ app/Jobs/ProcessCrawlingJob.php
@@ -9,13 +9,20 @@ use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\Middleware\WithoutOverlapping;
 use Illuminate\Queue\SerializesModels;
 
 class ProcessCrawlingJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    // the job definition
     protected $crawlingJob;
+    // timeout? for safety, set it at 30 minutes
+    public $timeout = 1800;
+    // force fail after timeout
+    public $failOnTimeout = true;
+    
     /**
      * Create a new job instance.
      *
@@ -42,5 +49,15 @@ class ProcessCrawlingJob implements ShouldQueue
         sleep(30);
 
         $service->update($this->crawlingJob, ['status' => 'DONE'], null);
+    }
+
+    /**
+     * Some middleware to prevent job overlapping
+     */
+    public function middleware() {
+        return [
+            // no release for duplicates, directly drop
+            (new WithoutOverlapping($this->crawlingJob->id))->dontRelease()
+        ];
     }
 }
